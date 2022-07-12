bf2aca467ce7ed1a28a7c2868e102ab45a086a91
David M <david.macias.0101@gmail.com>
2022-07-12 13:56:32 -0700

    setup the cli using click

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import click
 import scrape
 
 
+choice_diff = {"E": "Easy", "MD": "Intermediate", "H": "Hard"}
+
+
 @click.command()
-@click.option('--n', default=1, type=int, show_default=True, help="The number of questions generated")
-@click.option("--diff", default="EZ", type=click.Choice(['EZ', 'MD', 'H']),  help="The difficulty of the questions")
+@click.option('--n', default=1, type=click.IntRange(1, 10), show_default=True, help="The number of questions generated")
+@click.option("--diff", default="E", type=click.Choice(['E', 'MD', 'H']),  help="The difficulty of the questions")
 def hello(n, diff):
     num = int(n)
-    diff_lvl = str(diff)
+    lvl = choice_diff.get(str(diff))
+    r = scrape.Obj()
+    for _ in range(num):
+        submission = r.getRandomSubmission(lvl)
+        r.createFile(submission)
+    print("Done")
 
 
 if __name__ == '__main__':
