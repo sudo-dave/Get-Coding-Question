feae8ab2699f5cd66ef168989a41e7679586b4ed
David M <david.macias.0101@gmail.com>
2022-07-12 13:38:25 -0700

    cleaned up getRandom

--- scrape.py
+++ scrape.py
@@ -13,13 +13,32 @@ class Obj:
         self.user_agent = os.getenv("user_agent")
         r = praw.Reddit(
             client_id=self.client_id, client_secret=self.secret, user_agent=self.user_agent)
-        # self.reddit = r.subreddit("dailyprogrammer")
         self.reddit = r
 
-    def getRandomChallenge(self):
+    def getRandomSubmission(self, lvl=""):
         sub = None
         while True:
             submission = self.reddit.subreddit("dailyprogrammer").random()
-            if submission and "Challenges" not in submission and "Challenge" in submission:
+
+            title = submission.title
+
+            if "Challenges" not in title and lvl in title and "Challenge" in title:
                 sub = submission
                 break
+        return sub
+
+    def getFileName(self, sub):
+        left = sub.title.split("#")[1]
+        res = ""
+        for char in left:
+            if char == " ":
+                break
+            res += char
+
+        return "Q#" + res
+
+    def createFile(self, sub):
+        file_name = self.getFileName(sub)
+        with open(file_name + ".md", "w") as f:
+            f.write("# "+sub.title + "\n")
+            f.write(sub.selftext)
